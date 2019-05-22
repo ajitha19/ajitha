c993bef72e6946b2210fc374cde79409a99bc9cf
ajitha19 <50898643+ajitha19@users.noreply.github.com>
2019-05-22 17:38:20 +0530

    Update and rename python.py to ajitha.py

---
+++ ajitha.py
@@ -0,0 +1,7 @@
+a=int(input())
+if(a<0):
+  print("the num is Negative")
+elif(a>0):
+  print("the num is Positive")
+elif(a==0):
+  print("Zero")
--- python.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a num"))
-if(a<0):
-  print("the num is negative")
-elif(a>0):
-  print("the num is positive")
-elif(a==0):
-  print("the num is positive or negative")
-else:
-  print("nothing")
